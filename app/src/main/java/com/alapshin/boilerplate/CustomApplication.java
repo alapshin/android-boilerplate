c333ca91b6144b7fce7a998c9b4bcabbbcb81618
Andrei Lapshin <alapshin@fastmail.com>
2018-05-26 15:01:33 +0300

    Initial setup

---
+++ app/src/main/java/com/alapshin/boilerplate/CustomApplication.java
@@ -0,0 +1,66 @@
+package com.alapshin.boilerplate;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.Handler;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class CustomApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+
+        if (BuildConfig.DEBUG) {
+            setupLeakCanary();
+            setupStrictMode();
+            // Workaround for https://issuetracker.google.com/issues/36951662
+            new Handler().postAtFrontOfQueue(this::setupStrictMode);
+        }
+    }
+
+    protected void setupLeakCanary() {
+        LeakCanary.install(this);
+    }
+
+    protected void setupStrictMode() {
+        StrictMode.VmPolicy.Builder vmBuilder = new StrictMode.VmPolicy.Builder()
+                .penaltyLog()
+                .penaltyDeath()
+                .detectActivityLeaks()
+                .detectFileUriExposure()
+                .detectLeakedClosableObjects()
+                .detectLeakedRegistrationObjects()
+                .detectLeakedSqlLiteObjects();
+        if (Build.VERSION.SDK_INT >= 23) {
+            // Disabled due to 3rd party libraries
+            // vmBuilder.detectCleartextNetwork();
+            if (Build.VERSION.SDK_INT >= 26) {
+                vmBuilder.detectContentUriWithoutPermission();
+            }
+        }
+        StrictMode.setVmPolicy(vmBuilder.build());
+
+        StrictMode.ThreadPolicy.Builder threadBuilder = new StrictMode.ThreadPolicy.Builder()
+                .penaltyLog()
+                .penaltyDeath()
+                .detectNetwork()
+                // Disabled due to ResourceCompat#getFont()
+                //.detectDiskReads()
+                //.detectDiskWrites()
+                .detectCustomSlowCalls();
+        if (Build.VERSION.SDK_INT >= 23) {
+            threadBuilder.detectResourceMismatches();
+            if (Build.VERSION.SDK_INT >= 26) {
+                threadBuilder.detectUnbufferedIo();
+            }
+        }
+        StrictMode.setThreadPolicy(threadBuilder.build());
+    }
+}
